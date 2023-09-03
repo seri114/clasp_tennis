c88089f338409fe6fb52c3907c3f4b254a9fed31
seri <seri114@gmail.com>
2023-09-03 16:23:36 +0900

    複数の予定を追加できるよう修正

--- main.ts
+++ main.ts
@@ -11,78 +11,82 @@ function doGet(e: any) {
     return template.evaluate().setTitle("テニス予定追加").addMetaTag('viewport', 'width=device-width, initial-scale=1');
 }
 
-function getTodayTennisEvent(){
+function getTodayTennisEvents(){
     var date = new Date();
     var events = CalendarApp.getDefaultCalendar().getEventsForDay(date);
-    var event = events.find(function (event) {
+    var tennisEvents = events.filter(function (event) {
         return event.getTitle().indexOf("テニス") !== -1;
     });
-    return event;
+    return tennisEvents;
 }
-function isTennisAdded() {
-    return !!getTodayTennisEvent();
+
+function getTodayTennisEventsInfo(){
+    return getTodayTennisEvents().map((event)=>{
+        return {
+            title: event.getTitle()
+        }
+    })
 }
 
 function removeTodayTennis(){
-    const event = getTodayTennisEvent();
+    const events = getTodayTennisEvents();
+    var [,event] = getLatestTennisEvent(events);
     if(event){
+        const title = event.getTitle()
         event.deleteEvent();
-        return true;
+        return title;
     }
-    return false;
+    return "";
+}
+
+function getTennisNumber(event: GoogleAppsScript.Calendar.CalendarEvent){
+    var title = event.getTitle();
+    if (title.match(/^テニス\s*(\d+)$/)) {
+        var n = parseInt(RegExp.$1);
+        return n;
+    }
+    return undefined;
+}
+
+function getLatestTennisEvent(events: GoogleAppsScript.Calendar.CalendarEvent[]): [number, GoogleAppsScript.Calendar.CalendarEvent  | undefined]{
+    // テニスXXの数字を取得
+    var num = 0;
+    var event: GoogleAppsScript.Calendar.CalendarEvent | undefined = undefined;
+    for (var i = 0; i < events.length; i++) {
+        const n = getTennisNumber(events[i]);
+        if(n !== undefined){
+            if (n > num) {
+                num = n;
+                event = events[i]
+            }
+        }
+    }
+    return [num, event];
 }
 
 function addTennis() {
     var events = getRecentEvents()
 
     // テニスXXの数字を取得
-    var num = 0;
-    for (var i = 0; i < events.length; i++) {
-        var title = events[i].getTitle();
-        if (title.match(/^テニス\s*(\d+)$/)) {
-            var n = parseInt(RegExp.$1);
-            if (n > num) {
-                num = n;
-            }
-        }
-    }
+    var [num,] = getLatestTennisEvent(events);
     num++;
 
     // 新しい予定のタイトルを作成
     var title = "テニス" + num;
 
-    // 土曜日は「本村小」、日曜日は「南山小」に設定
-    var dayOfWeek = new Date().getDay();
-    var location = "";
-    if (dayOfWeek == 6) {
-        location = "本村小";
-    } else if (dayOfWeek == 0) {
-        location = "南山小";
-    } else {
-        // if(!yesno('土日以外ですが、予定を追加しますか？')){
-        //   return createAlert('予定の追加をスキップしました。')
-        // }
-        location = "南山小";
-    }
-
-    var hasTennisEvent = isTennisAdded();
-    if (hasTennisEvent) {
-        return 'すでに予定を追加済です。';
-    }
-
     // 予定を作成
     var startTime = new Date();
     startTime.setHours(8, 55, 0, 0);
     var endTime = new Date(startTime.getTime() + 85 * 60 * 1000);
-    var event = CalendarApp.getDefaultCalendar().createEvent(title, startTime, endTime, { location: location });
+    var event = CalendarApp.getDefaultCalendar().createEvent(title, startTime, endTime);
 
-    // ポップアップ表示の必要性を判定し、Webページを生成する
+    // 振り込み必要？
     var needBankTransfer = false;
     if (num % 8 == 7) {
         needBankTransfer = true;
     }
-    // var html = "<html><body>新しい予定が追加されました<br>" + event.getTitle() + "<br>" + event.getStartTime() + "<br>" + event.getLocation() + "<br>" + popup + "</body></html>";
-    // return HtmlService.createHtmlOutput(html);
-    var html = `${needBankTransfer ? "振り込みが必要です!!!" : ""}新しい予定が追加されました。 ${event.getTitle()} ${event.getLocation()}`;
-    return html;
+    return {
+        info: `${event.getTitle()} を追加しました。`,
+        warn: needBankTransfer ? "振り込みが必要です!!!" : ""
+    }
 }
